c22ee21fb44348cbb816a0b1b033e0c8160b79bb
promecarus <haikalslipi@gmail.com>
2023-10-29 18:29:44 +0700

    finish

--- app/src/androidTest/java/com/promecarus/mycomposetesting/CalculatorAppTest.kt
+++ app/src/androidTest/java/com/promecarus/mycomposetesting/CalculatorAppTest.kt
@@ -1,6 +1,7 @@
 package com.promecarus.mycomposetesting
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
@@ -11,18 +12,30 @@ import org.junit.Test
 
 class CalculatorAppTest {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val rule = createAndroidComposeRule<ComponentActivity>()
 
     @Before
     fun setUp() {
-        composeTestRule.setContent { MyComposeTestingTheme { CalculatorApp() } }
+        rule.setContent { MyComposeTestingTheme { CalculatorApp() } }
     }
 
     @Test
     fun calculate_area_of_rectangle_correct() {
-        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
-        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
-        composeTestRule.onNodeWithText("Hitung!").performClick()
-        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+        rule.apply {
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, 12.0)).assertExists()
+        }
+    }
+
+    @Test
+    fun wrong_input_not_calculated() {
+        rule.apply {
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("..3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, 0.0)).assertExists()
+        }
     }
 }
